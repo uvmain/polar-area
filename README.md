ffd40600c10857ea15b3b9ab0914286c00dce891
Ian Baron <ian.baron@manchester.ac.uk>
2023-10-12 11:13:36 +0100

    dockerfile, readme updates

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-# Vue 3 + Vite
+# Local
+```
+npm run dev
+```
 
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+# Docker
+```
+docker build -t polararea:latest -f Dockerfile .
+docker run -p 8084:8084 -d polararea:latest
+```
 
-## Recommended IDE Setup
-
-- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+# Azure Container Instance
+```
+az container create --resource-group rg-microservices --name polararea --image polararea:latest --cpu 1 --memory 1 --os-type Linux --ip-address Public --ports 8084 --dns-name-label portalarea
+```
--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { onMounted, ref, defineProps, watchEffect } from 'vue'
+import { onMounted, ref, watchEffect } from 'vue'
 import Chart from 'chart.js/auto'
 import uniqolor from 'uniqolor'
 
-let chartInstance = null; // Store the chart instance
+let chartInstance = null
 
 const props = defineProps({
   inputs: {
@@ -18,9 +18,9 @@ const datasets = []
 const colours = []
 
 function generateData() {
-  labels.length = 0;
-  datasets.length = 0;
-  colours.length = 0;
+  labels.length = 0
+  datasets.length = 0
+  colours.length = 0
   for (const input of props.inputs) {
     labels.push(input.label)
     datasets.push(input.value)
@@ -35,7 +35,7 @@ function generateData() {
 
 async function generateChart() {
   if (chartInstance) {
-    chartInstance.destroy(); // Destroy the previous chart instance if it exists
+    chartInstance.destroy()
   }
   chartInstance = new Chart(
     document.getElementById('chart'),
@@ -74,14 +74,13 @@ async function generateChart() {
       },
     }
   )
-};
+}
 
 onMounted(() => {
   generateData()
   generateChart()
 })
 
-// Watch for changes in the inputs prop and update the chart data
 watchEffect(() => {
   generateData()
   generateChart()
